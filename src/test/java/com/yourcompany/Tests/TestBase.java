1fc84b49c97d15d40d318b7f90cdc24b097787fe
gilda-ab <78222870+gilda-ab@users.noreply.github.com>
2024-04-24 11:20:50 +0100

    Update TestBase.java

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -91,7 +91,7 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
     public static LinkedList browsersStrings() {
         LinkedList browsers = new LinkedList();
 
-        browsers.add(new String[]{"Windows 10", "14.14393", "MicrosoftEdge", null, null});
+        browsers.add(new String[]{"Windows 10", "123.0", "MicrosoftEdge", null, null});
 //         browsers.add(new String[]{"Windows 10", "49.0", "firefox", null, null});
 //         browsers.add(new String[]{"Windows 7", "11.0", "internet explorer", null, null});
 //         browsers.add(new String[]{"OS X 10.11", "10.0", "safari", null, null});
